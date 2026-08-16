015c64dde21dd96875026b7525567848c6bdc6aa
Pomboverso <github.5yeil@simplelogin.fr>
2026-08-16 04:30:01 -0700

    add version and help flags

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ mod util;
 use std::io;
 
 fn main() -> io::Result<()> {
+    if handle_cli_flags() {
+        return Ok(());
+    }
+
     let mut terminal = ratatui::init();
 
     app::run(&mut terminal)?;
@@ -19,4 +23,20 @@ fn main() -> io::Result<()> {
     ratatui::restore();
 
     Ok(())
+}
+
+fn handle_cli_flags() -> bool {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    if args.iter().any(|a| a == "--version" || a == "-V" || a == "-v") {
+        println!("jaiba {}", env!("CARGO_PKG_VERSION"));
+        return true;
+    }
+
+    if args.iter().any(|a| a == "--help" || a == "-h") {
+        println!("jaiba {}\n\nA terminal password manager built on the KeePass (.kdbx) file format.\n\nUSAGE:\n    jaiba [OPTIONS]\n\nOPTIONS:\n    -V, --version    Print version information\n    -h, --help       Print this help message", env!("CARGO_PKG_VERSION"));
+        return true;
+    }
+
+    false
 }
